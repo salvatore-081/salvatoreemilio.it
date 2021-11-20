61da7cafe6378d80fbc6030c6f518bb13b7d2f62
Salvatore Emilio <razielsasy@gmail.com>
2021-11-20 17:44:07 +0100

    fixed dockerfile cmd and main port

--- api/public/main.py
+++ api/public/main.py
@@ -2,5 +2,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("rest.v1.api:app", host='0.0.0.0', port=os.getenv("PORT", default=14020),
-                reload=False, debug=True, workers=2)
+    uvicorn.run("rest.v1.api:app", host='0.0.0.0', port=int(
+        os.getenv("PORT", default=14020)), workers=2)
