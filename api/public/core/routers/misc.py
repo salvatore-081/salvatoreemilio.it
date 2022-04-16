c62023f6af8537af07977af78aa47349cec814f7
Salvatore <razielsasy@gmail.com>
2022-04-16 07:12:05 +0000

    added nginx

--- api/public/core/routers/misc.py
+++ api/public/core/routers/misc.py
@@ -5,8 +5,9 @@ from models.misc import WhatsMyIp
 def getMiscRouter():
     router = APIRouter()
 
-    @router.get("/whatsmyip", response_model=WhatsMyIp, response_model_exclude_none=True)
+    @router.get("/whatsmyip", 
+    response_model=WhatsMyIp, response_model_exclude_none=True)
     async def get_whats_my_ip(request: Request):
-        return {"ip": request.client.host}
+        return {"ip": request.headers.get("x-forwarded-for").split(", ")[0]}
 
     return router
